606106126ba77f33f159683483e376dbdda281fa
Johannes-B-stock <52247101+Johannes-B-stock@users.noreply.github.com>
2022-08-13 13:32:06 +0200

    Setup for any email provider

--- config/plugins.js
+++ config/plugins.js
@@ -54,12 +54,18 @@ module.exports = ({ env }) => ({
     },
   },
   email: {
-    config: {
-      provider: "sendmail",
-      settings: {
-        defaultFrom: env("DEFAULT_EMAIL_FROM"),
-        defaultReplyTo: env("DEFAULT_EMAIL_REPLYTO"),
+    provider: env("EMAIL_PROVIDER"),
+    providerOptions: {
+      host: env("EMAIL_SMTP_HOST", "smtp.example.com"),
+      port: env("EMAIL_SMTP_PORT", 587),
+      auth: {
+        user: env("EMAIL_SMTP_USER"),
+        pass: env("EMAIL_SMTP_PASS"),
       },
+    },
+    settings: {
+      defaultFrom: env("DEFAULT_EMAIL_FROM"),
+      defaultReplyTo: env("DEFAULT_EMAIL_REPLYTO"),
     },
   },
   // ...
